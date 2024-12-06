8624478fddf97051fbde5711e02637ca3e5ce37c
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2024-12-06 22:08:03 +0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div className="bg-brand text-brand-950">Home</div>;
-}
+import Header from '../components/Header';
+import CategorySection from '../components/CategorySection';
+import ProductSection from '../components/ProductSection';
+import TestimonialSection from '../components/TestimonialSection';
+import ContactSection from '../components/ContactSection';
+import HeroSection from '@/components/HeroSection';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <HeroSection/>
+      <CategorySection />
+      <ProductSection />
+      <TestimonialSection />
+      <ContactSection />
+    </div>
+  );
+};
+
+export default Home;
