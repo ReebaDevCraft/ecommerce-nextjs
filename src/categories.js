3b03ac88f59422a2c63340391d54e857471f75d9
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2024-12-27 22:46:15 +0500

    Add files via upload

---
+++ src/categories.js
@@ -0,0 +1,7 @@
+const categories = [
+{name:"Bags", image: "/images/bags.jpg"},
+{name:"Shoes", image: "/images/shoes.jpg"},
+{name:"Coats", image: "/images/coats.jpg"},
+{name:"Glasses", image: "/images/glasses.jpg"},
+];
+export default categories;
